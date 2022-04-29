7d83a5b7702b5e92473a1aaed4c4af318bc1039c
DaCuteRaccoon stands with Ukraine <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-04-29 09:23:37 -0700

    Create Lisp.lisp

---
+++ archive/Lisp.lisp
@@ -0,0 +1,8 @@
+(defun fizzBuzz ()
+  (loop for x from 1 to 100 do
+        (princ (cond ((zerop (mod x 15)) "FizzBuzz")
+                 ((zerop (mod x 3)) "Fizz")
+                 ((zerop (mod x 5)) "Buzz")
+                 (t x)))
+        (terpri)))
+(fizzBuzz)
